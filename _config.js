62afaba41964248ff40cdf25cf9f767e86dc6b2b
dankip <rotichtitus16@gmail.com>
2023-07-05 15:17:48 +0300

    database setup

---
+++ _config.js
@@ -0,0 +1,6 @@
+config.mongoURI = {
+    production: 'mongodb+srv://dankip:@Sirdancun12@cluster0.ntf17ql.mongodb.net/gallery?retryWrites=true&w=majority',
+    development: 'mongodb+srv://dankip:@Sirdancun12@cluster0.ntf17ql.mongodb.net/gallery-dev?retryWrites=true&w=majority',
+    test: 'mongodb+srv://dankip:@Sirdancun12@cluster0.ntf17ql.mongodb.net/gallery-test?retryWrites=true&w=majority',
+}
+module.exports = config;
